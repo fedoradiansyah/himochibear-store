580c07d5972b1dc1c416e2c19816efe2dc898b0c
ferdiansyahpermananinditha <fedoradiansyah@gmail.com>
2021-03-21 01:19:30 +0700

    first commit

---
+++ database/seeds/IndoRegionDistrictSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use AzisHapidin\IndoRegion\RawDataGetter;
+use Illuminate\Support\Facades\DB;
+class IndoRegionDistrictSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+          $districts = RawDataGetter::getDistricts();
+
+        // Insert Data to Database
+        DB::table('districts')->insert($districts);
+    }
+}
